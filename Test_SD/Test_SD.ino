f0f73793760831895e37f795c982999f1aade884
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2022-02-23 13:07:17 -0800

    log to a binary format instead of text for performance

--- Test_SD/Test_SD.ino
+++ Test_SD/Test_SD.ino
@@ -17,11 +17,6 @@ unsigned long start_time;
 int x=0;
 unsigned long startertime;
 
-float gyro[3];
-float accel[3];
-
-int gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z; 
-
 void setup() {
 
   //I2C_Init();
@@ -72,71 +67,38 @@ void setup() {
     while(1);
   }
   filep.close();
-  //filep = SD.open("imu4.txt", FILE_WRITE);
-//  SD.remove("imu4.txt");
 }
+
 void loop() {
-  filep = SD.open("imu4.txt", FILE_WRITE);
+  filep = SD.open("imu.bin", FILE_WRITE);
   startertime = millis();
   for(int i=0;i<100;i++){
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     start_time = millis();
     if(start_time - startertime >= 100){
       startertime=start_time;
       mag.read();
     }
-//    Serial.println("Gyro");
     gyro_acc.readGyro();
-//   gyro_x = gyro_acc.g.x;
-//   gyro_y = gyro_acc.g.y;
-//   gyro_z = gyro_acc.g.z;
-
-//    Serial.println("Accel");
     gyro_acc.readAcc();
-//   accel_x = gyro_acc.a.x >> 4;
-//   accel_y = gyro_acc.a.y >> 4;
-//   accel_z = gyro_acc.a.z >> 4;
-  
-    //x
-    accel[0] = gyro_acc.a.x;
-    //y
-    accel[1] = gyro_acc.a.y;
-    //z
-    accel[2] = gyro_acc.a.z;
-
-    //x
-    gyro[0] = gyro_acc.g.x;
-    //y
-    gyro[1] = gyro_acc.g.y;
-    //z
-    gyro[2] = gyro_acc.g.z;
-
     long pressure = ps.readPressureRaw();
     int temp = ps.readTemperatureRaw();
-
-//    start_time=start_time+","+accel+","+gyro;
-//    filep.print(start_time);
-//    filep.print(",");
-    filep.println((String)start_time+","+accel[0]+","+accel[1]+","+accel[2]+","+gyro[0]+","+gyro[1]+","+gyro[2] + "," + pressure + "," + temp + "," + mag.m.x + "," + mag.m.y + "," + mag.m.z);
-//    filep.print(accel[1]);
-//    filep.print(",");
-//    filep.print(accel[2]);
-//    filep.print(",");
-//    filep.print(gyro[0]);
-//    filep.print(",");
-//    filep.print(gyro[1]);
-//    filep.print(",");
-//    filep.println(gyro[2]);
-//    Serial.println(millis());
+    struct {
+      uint32_t marker;
+      uint32_t time;
+      int32_t pressure;
+      int16_t xacc, yacc, zacc;
+      int16_t xgy, ygy, zgy;
+      int16_t xmag, ymag, zmag;
+      int16_t temp;
+    } packet = {
+      0x5555AAAA, currentTime, pressure,
+      gyro_acc.a.x, gyro_acc.a.y, gyro_acc.a.z,
+      gyro_acc.g.x, gyro_acc.g.y, gyro_acc.g.z,
+      mag.m.x, mag.m.y, mag.m.z,
+      temp
+    };
+    filep.write((uint8_t*)&packet, sizeof(packet));
   }
-  //start_time = millis();
-//  filep = SD.open("imu2.txt", FILE_WRITE);
-//  filep.print(start_time);
-//  filep.print(",");
-//  filep.print(accel);
-//  filep.print(",");
-//  filep.println(gyro);
- // start_time=start_time+","+accel+","+gyro;
-//  filep.println(start_time);
-//  Serial.println(millis());
   filep.close();
 }
